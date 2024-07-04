eb7827985ef222b5416682b62a2a3cff6bc1b47f
Jacob Engelbreth <jacob.josias95@gmail.com>
2024-07-04 20:05:05 +0200

    last change

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-function randomCharacter() {
-  return characters[Math.floor(Math.random() * characters.length)];
+let passwordLength = 12
+
+function getRandomCharacter() {
+    let randomChar = Math.floor(Math.random() * characters.length)
+    return characters[randomChar]
 }
 
-console.log(randomCharacter());
+function generateRandomPassword() {
+    let randomPassword = ""
+    for (let i = 0; i < passwordLength; i++) {
+        randomPassword += getRandomCharacter()         
+    }
+    return randomPassword
+}
+
+const generatedPasswordOne = generateRandomPassword()
+
+document.getElementById("generate-password-btn").addEventListener("click", function() {
+    const password = generateRandomPassword();
+    document.getElementById("password-display1").innerText = password;
+    document.getElementById("password-display2").innerText = password;
+});
